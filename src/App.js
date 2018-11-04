bc8cfd4b6a640187d363c172f032588a93407937
v_lishaohai <baidu@123lsh.com>
2018-11-04 15:00:09 +0800

    react-create-app use mobx mobx-react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-
+import {inject, observer} from 'mobx-react';
+@inject('BirdStore')
+@observer
 class App extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('this.bird :', this.bird);
+    const bird = this.bird.value;
+    this.props.BirdStore.addBird(bird);
+    this.bird.value = '';
+  };
   render() {
+    const {BirdStore} = this.props;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>hello world</h1>
+        <h2>You have {BirdStore.birdCount} birds</h2>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Enter Bird"
+            ref={input => (this.bird = input)}
+          />
+          <button>Add bird</button>
+        </form>
+        <ul>
+          {BirdStore.birds.map(bird => (
+            <li key={bird}>{bird}</li>
+          ))}
+        </ul>
       </div>
     );
   }
---
+++ src/stores/BirdStore.js
@@ -0,0 +1,15 @@
+import {observable, action, computed} from 'mobx';
+class BirdStore {
+  @observable
+  birds = [];
+  @action
+  addBird = bird => {
+    return this.birds.push(bird);
+  };
+  @computed
+  get birdCount() {
+    return this.birds.length;
+  }
+}
+const store = new BirdStore();
+export default store;
